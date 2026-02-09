06869b4e60c8a6942da963bd34f83b729db2210b
Eivind <ehaugw@gmail.com>
2026-02-09 13:43:26 +0100

    commit 3.11 working pads

--- Makefile
+++ Makefile
@@ -1,26 +1,28 @@
 include Makefile.helpers
+include Makefile.secret
 modname = non-ugly-cpc
 dependencies =
 
-assemble:
+unity-from:
+	cp /mnt/c/Users/eivind/MYHOME/Documents/SPTModding/SDK/EscapeFromTarkov-SDK/AssetBundles/StandaloneWindows/harris_adapter.bundle bundles
+
+assemble: unity-from
 	rm -f -r export
 	mkdir -p export/$(tspath)/$(modname)
 	mkdir -p export/$(tspath)/$(modname)/src
 	mkdir -p export/$(tspath)/$(modname)/bundles
-	mkdir -p export/$(tspath)/$(modname)/db
 	cp -r src/* export/$(tspath)/$(modname)/src
 	cp -r bundles/* export/$(tspath)/$(modname)/bundles
-	cp -r db/* export/$(tspath)/$(modname)/db
 	cp package.json export/$(tspath)/$(modname)/
 	cp bundles.json export/$(tspath)/$(modname)/
 	cp README.md export/$(tspath)/$(modname)/
 	cp icon.png export/$(tspath)/$(modname)/
-	cp disclaimer.pdf export/$(tspath)/$(modname)/
 
 forceinstall:
 	make assemble
 	rm -r -f $(gamepath)/$(pluginpath)/$(modname)
 	cp -u -r export/* $(gamepath)
+	scp -r export/* $(hostwanuser):SPT_3_11/server_files
 
 play:
 	(make install && cd .. && make play)
--- src/mod.ts
+++ src/mod.ts
@@ -1,123 +1,27 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
-import * as fs from "fs";
-import * as path from "path";
-
 import { DependencyContainer } from "tsyringe";
+
+import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
-import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
-import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
+import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
-// WTT imports
-import { WTTInstanceManager } from "./WTTInstanceManager";
-import { CustomItemService } from "./CustomItemService";
-import { epicItemClass } from  "./EpicsEdits"
-// Custom Trader Assort Items
-import { CustomAssortSchemeService } from "./CustomAssortSchemeService";
-import { CustomWeaponPresets } from "./CustomWeaponPresets";
+import { CustomItemService } from "@spt/services/mod/CustomItemService";
+import { NewItemFromCloneDetails } from "@spt/models/spt/mod/NewItemDetails";
 
 
+const cryeCpcGoons = "628b9c7d45122232a872358f";
+const knightMask = "62963c18dbc8ab5f0d382d0b";
+const magpul_bipod_mount = "671126b049e181972e0681fa";
 
-class WTT_Test
-implements IPreSptLoadMod, IPostDBLoadMod
-{
-    private Instance: WTTInstanceManager = new WTTInstanceManager();
-    private version: string;
-    private modName = "RSASS HG Black";
-    private config;
-
-    private customItemService: CustomItemService = new CustomItemService();
-    private epicItemClass: epicItemClass = new epicItemClass();
-    private customAssortSchemeService: CustomAssortSchemeService = new CustomAssortSchemeService();
-    private customWeaponPresets: CustomWeaponPresets = new CustomWeaponPresets();
-
-    debug = false;
-
-    // Anything that needs done on preSptLoad, place here.
-    public preSptLoad(container: DependencyContainer): void 
+class Mod implements IPostDBLoadMod
+{    
+    public postDBLoad(container: DependencyContainer): void
     {
-    // Initialize the instance manager DO NOTHING ELSE BEFORE THIS
-        this.Instance.preSptLoad(container, this.modName);
-        this.Instance.debug = this.debug;
-        // EVERYTHING AFTER HERE MUST USE THE INSTANCE
-        
-        this.getVersionFromJson();
-        this.displayCreditBanner();
-
-        this.customItemService.preSptLoad(this.Instance);
-
-        this.epicItemClass.preSptLoad(this.Instance);
-        this.customAssortSchemeService.preSptLoad(this.Instance);
-
-        this.customWeaponPresets.preSptLoad(this.Instance);
-
-    }
-
-    // Anything that needs done on postDBLoad, place here.
-    postDBLoad(container: DependencyContainer): void 
-    {
-    // Initialize the instance manager DO NOTHING ELSE BEFORE THIS
-        this.Instance.postDBLoad(container);
-        // EVERYTHING AFTER HERE MUST USE THE INSTANCE
-
-        this.epicItemClass.postDBLoad();
-        this.customItemService.postDBLoad();
-        this.customAssortSchemeService.postDBLoad();
-        this.customWeaponPresets.postDBLoad();
-
-        this.Instance.logger.log(
-            `[${this.modName}] Database: Loading complete.`,
-            LogTextColor.GREEN
-        );
-    }
-
-    private getVersionFromJson(): void 
-    {
-        const packageJsonPath = path.join(__dirname, "../package.json");
-
-        fs.readFile(packageJsonPath, "utf-8", (err, data) => 
-        {
-            if (err) 
-            {
-                console.error("Error reading file:", err);
-                return;
-            }
-
-            const jsonData = JSON.parse(data);
-            this.version = jsonData.version;
-        });
-    }
-
-    public colorLog(message: string, color: string) {
-        const colorCodes = {
-            red: "\x1b[31m",
-            green: "\x1b[32m",
-            yellow: "\x1b[33m",
-            blue: "\x1b[34m",
-            magenta: "\x1b[35m",
-            cyan: "\x1b[36m",
-            white: "\x1b[37m",
-            gray: "\x1b[90m",
-            brightRed: "\x1b[91m",
-            brightGreen: "\x1b[92m",
-            brightYellow: "\x1b[93m",
-            brightBlue: "\x1b[94m",
-            brightMagenta: "\x1b[95m",
-            brightCyan: "\x1b[96m",
-            brightWhite: "\x1b[97m"
-        };
-      
-        const resetCode = "\x1b[0m";
-        const colorCode = colorCodes[color as keyof typeof colorCodes] || "\x1b[37m"; // Default to white if color is invalid.
-        console.log(`${colorCode}${message}${resetCode}`); // Log the colored message here
-    }
-
-    private displayCreditBanner(): void 
-    {
-        this.colorLog
-        (`[${this.modName}] Developers:  EpicRangeTime, GMAK3R   Code Framework: GroovypenguinX
-                                                        The RSASS handguard comes in black now`, "green");
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        const tables: IDatabaseTables = databaseServer.getTables();
+        tables.templates.items[cryeCpcGoons]._props.Prefab.path = "cpc_no_pads.bundle";
+        tables.templates.items[knightMask]._props.Prefab.path = "maskfix.bundle";
+        tables.templates.items[magpul_bipod_mount]._props.Prefab.path = "harris_adapter.bundle";
     }
 }
 
-module.exports = { mod: new WTT_Test() };
+export const mod = new Mod();
